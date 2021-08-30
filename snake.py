6fbe856f67e0565782210be53ca1bf2d47bc3364
Yogeshwar Chaturvedi <kanhay8@gmail.com>
2021-08-30 16:37:37 +0530

    import correction

--- snake.py
+++ snake.py
@@ -1,7 +1,7 @@
 from turtle import *
 from random import randrange
 from freegames import square
-from freegames.utils import square, vector
+from freegames import square, vector
 
 food = vector(0, 0)
 snake = [vector(10, 0)]
